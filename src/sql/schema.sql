148fc5586115250b588bed944d41126a44a66ca3
zycro21 <dimaszycro10@gmail.com>
2026-05-19 05:15:10 +0700

    fix: schema.sql

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,35 +1,26 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     profile_image TEXT,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE balances (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-
     balance BIGINT DEFAULT 0,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE banners (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     banner_name VARCHAR(255) NOT NULL,
-
     banner_image TEXT NOT NULL,
-
     description TEXT,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -72,17 +63,11 @@ VALUES
 
 CREATE TABLE services (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     service_code VARCHAR(100) UNIQUE NOT NULL,
-
     service_name VARCHAR(255) NOT NULL,
-
     service_icon TEXT,
-
     service_tariff BIGINT NOT NULL CHECK (service_tariff >= 0),
-
     description TEXT,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -168,21 +153,14 @@ VALUES
 
 CREATE TABLE transactions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     invoice_number VARCHAR(100) UNIQUE NOT NULL,
-
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-
     service_code VARCHAR(100)
     REFERENCES services(service_code),
-
     transaction_type VARCHAR(20) NOT NULL
     CHECK (transaction_type IN ('PAYMENT', 'TOPUP')),
-
     total_amount BIGINT NOT NULL
     CHECK (total_amount >= 0),
-
     description TEXT,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
